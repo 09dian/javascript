bbdb712b06795f5218d8c4ba9022a2b22833ee4e
09dian <09dianm@gmail.com>
2025-05-15 11:40:40 +0700

    update

---
+++ app.js
@@ -0,0 +1,57 @@
+function satu() {
+ const layar = document.querySelector(".layar");
+ let isi =layar.innerText;
+ layar.innerText = (isi=="0") ? "1" : isi + "1";
+}
+function dua() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "2" : isi + "2";
+}
+
+function tiga() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "3" : isi + "3";
+}
+
+function empat() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "4" : isi + "4";
+}
+
+function lima() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "5" : isi + "5";
+}
+
+function enam() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "6" : isi + "6";
+}
+
+function tujuh() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "7" : isi + "7";
+}
+
+function delapan() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "8" : isi + "8";
+}
+
+function sembilan() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "9" : isi + "9";
+}
+function kosong() {
+    const layar = document.querySelector(".layar");
+    let isi =layar.innerText;
+    layar.innerText = (isi=="0") ? "0" : isi + "0";
+}
